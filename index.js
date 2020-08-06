db155297622992ec79a508e72a57f9364b0b2410
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-06 14:58:03 -0400

    add post

--- index.js
+++ index.js
@@ -2,9 +2,27 @@
 const express = require('express');
 const app = express();
 
-//* Get Route - Home
+//* Route Handler - Get Request
+// POST configs form for the browser 
+// Browser takes all the information 
+// And send a post request to the same route
 app.get('/', (req, res) => {
-  res.send('Hi There');
+  res.send(`
+    <div>
+      <form method="POST">
+        <input name="email" placeholder ="email" />
+        <input name= "password" placeholder ="password" />
+        <input name= "passwordConfirmation" placeholder ="password confirmation" />
+        <button>Sign Up</button>
+      </form>
+    </div>
+  `);
+});
+
+//* Router Handler - Post Request
+// Information is stored into req.body assigned to their name attribute
+app.post('/', (req, res) => {
+  res.send(`Account Created`);
 });
 
 //* Start up server
