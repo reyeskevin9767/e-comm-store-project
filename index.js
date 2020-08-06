562c435ce8add78d5add603bc59200138c695bab
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-06 12:03:47 -0400

    setup server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hi World');
+//* Create a web server
+const express = require('express');
+const app = express();
+
+//* Get Route - Home
+app.get('/', (req, res) => {
+  res.send('Hi There');
+});
+
+//* Start up server
+app.listen(3000, () => {
+  console.log('Listening');
+});
