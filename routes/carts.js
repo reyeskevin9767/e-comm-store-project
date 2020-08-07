86c67b2182f89b7e4daa982efbda21e7fc1d0d7d
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-07 19:21:13 -0400

    add cartshowtemplate and html and css

--- routes/carts.js
+++ routes/carts.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cartsRepo = require('../repositories/carts');
-const carts = require('../repositories/carts');
+const productsRepo = require('../repositories/products');
+const cartShowTemplate = require('../views/carts/show');
 
 const router = express.Router();
 
+//* Cart Post Route
 // Recieve a post request to add an item to a cart
 router.post('/cart/products', async (req, res) => {
   let cart;
@@ -34,8 +36,23 @@ router.post('/cart/products', async (req, res) => {
   res.send('Product Addded To Cart');
 });
 
+//* Cart SHOW ROUTE
 // Recieve a get request to show all items in cart
+router.get('/cart', async (req, res) => {
+  if (!req.session.cartId) {
+    return res.redirect('/');
+  }
 
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  for (let item of cart.items) {
+    const product = await productsRepo.getOne(item.id);
+
+    item.product = product;
+  }
+
+  res.send(cartShowTemplate({ items: cart.items }));
+});
 // REceive a post request to delete an item from cart
 
 module.exports = router;
