bbb4a5060ab19a005a9b771b7ff07403355c94f6
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-07 02:26:31 -0400

    create products repository

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,11 +1,10 @@
 const express = require('express');
+const productsRepo = require('../../repositories/products');
 
 const router = express.Router();
 
 //* Products SHOW Route
-router.get('/admin/products', (req, res) => {
-
-});
+router.get('/admin/products', (req, res) => {});
 
 //* Products NEW Route
 router.get('/admin/products/new', (req, res) => {});
