2faa63972cfa9bcd40770a78114a14646087ea9f
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-07 17:41:25 -0400

    update css and html of user products page

--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,7 @@ const productsRepo = require('../repositories/products');
 const productsIndexTemplate = require('../views/products/index');
 const router = express.Router();
 
-//* User Products SHOW Route
+//* User Products GET Route
 router.get('/', async (req, res) => {
   const products = await productsRepo.getAll();
   res.send(productsIndexTemplate({ products }));
