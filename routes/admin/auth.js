a0eeadc19a285e48de3422a192c2e6335f30938a
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-07 15:20:13 -0400

    create resuseable error handling middleware

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator');
+const { handleErrors } = require('./middlewares');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -25,14 +25,8 @@ router.get('/signup', (req, res) => {
 router.post(
   '/signup',
   [requireEmail, requirePassword, requirePasswordConfirmation],
+  handleErrors(signupTemplate),
   async (req, res) => {
-    // Results from validation is sent to req
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.send(signupTemplate({ req, errors }));
-    }
-
     // All the data is store in req.body
     const { email, password } = req.body;
 
@@ -61,15 +55,11 @@ router.get('/signin', (req, res) => {
 router.post(
   '/signin',
   [requireEmailExists, requireValidPasswordForUser],
+  handleErrors(signinTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-    
-    if(!errors.isEmpty()){
-      return res.send(signinTemplate({errors}))
-    }
-
     const { email } = req.body;
 
+    // Find the user 
     const user = await usersRepo.getOneBy({ email });
 
     req.session.userId = user.id;
