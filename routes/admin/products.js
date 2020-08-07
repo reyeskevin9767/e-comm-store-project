81c27390b523340736e6915d6ca2cdb925e3e10f
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-07 02:38:47 -0400

    add products new route

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const productsRepo = require('../../repositories/products');
-
+const productsNewTemplate = require('../../views/admin/products/new');
 const router = express.Router();
 
 //* Products SHOW Route
 router.get('/admin/products', (req, res) => {});
 
 //* Products NEW Route
-router.get('/admin/products/new', (req, res) => {});
+router.get('/admin/products/new', (req, res) => {
+  res.send(productsNewTemplate({}));
+});
 
 module.exports = router;
