392b5329c9e75c9261a5794c22ff98beed7f9091
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-07 15:03:10 -0400

    fix bug with middleware order

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -21,10 +21,15 @@ router.get('/admin/products/new', (req, res) => {
 // Not The best way to save images, avoid in production applications
 router.post(
   '/admin/products/new',
-  [requireTitle, requirePrice],
   upload.single('image'),
+  [requireTitle, requirePrice],
   async (req, res) => {
     const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.send(productsNewTemplate({ errors }));
+    }
+
     const image = req.file.buffer.toString('base64');
     const { title, price } = req.body;
 
