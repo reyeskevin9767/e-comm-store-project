d0e92d43523c4db2ecfc559a771d75ad42c5119a
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-07 02:12:53 -0400

    create products routes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/products');
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.use(
 
 //* Run routes from other files
 app.use(authRouter);
+app.use(productsRouter);
 
 //* Start up server
 app.listen(3000, () => {
---
+++ routes/admin/products.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+//* Products GET Route
+router.get('/admin/products', (req, res) => {
+  
+});
+
+//* Products NEW ROUTE
+router.get('/admin/products/new', (req, res) => {});
+
+module.exports = router;
