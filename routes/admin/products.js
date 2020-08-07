aa085a3e6ec01f719ca3f584cfbe095b44f97ece
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-07 16:09:02 -0400

    create products edit route

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -5,6 +5,7 @@ const { handleErrors, requireAuth } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const productsIndexTemplate = require('../../views/admin/products/index');
+const productsEditTemplate = require('../../views/admin/products/edit');
 const { requireTitle, requirePrice, requireImage } = require('./validators');
 const router = express.Router();
 
@@ -38,5 +39,17 @@ router.post(
     res.redirect('/admin/products');
   }
 );
+
+//* Products Edit Route
+router.get('/admin/products/:id/edit', (req, res) => {
+  const product = await productsRepo.getOne(req.params.id)
+
+  if(!product){
+    return res.send('Product not found')
+  }
+
+  res.send(productsEditTemplate({product}))
+
+});
 
 module.exports = router;
